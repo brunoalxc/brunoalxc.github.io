9f9325148cb635c62c10582aac1ff14dae1d1090
brunoalx <bruno.alex2020.ba@gmail.com>
2025-09-19 15:39:47 -0300

    php site init

--- js/script.js
+++ js/script.js
@@ -1,111 +1,126 @@
 // Dados simulados dos cursos
         const cursos = {
-    // Cursos de Graduação EAD
-    graduacao: [
-        { 
-            titulo: "História - Licenciatura", 
-            desc: "Curso de História com foco em metodologias ativas.", 
-            img: "",
-            url: "https://app.faesma.com.br/captacao/public/selecao-curso"
-        },
-        { 
-            titulo: "Pedagogia - Licenciatura", 
-            desc: "Formação completa em Pedagogia, para formar professores renomados.", 
-            img: "img/PEDAGOGIA.png",
-            url: "https://app.faesma.com.br/captacao/public/selecao-curso"
-        },
-        { 
-            titulo: "Educação Especial - Licenciatura", 
-            desc: "Educação Especial com ênfase em inclusão social.", 
-            img: "",
-            url: "https://app.faesma.com.br/captacao/public/selecao-curso"
-        },
-        { 
-            titulo: "Educação Física - Bacharelado", 
-            desc: "Educação Física para formação de profissionais capacitados.", 
-            img: "",
-            url: "https://app.faesma.com.br/captacao/public/selecao-curso"
-        },
-        { 
-            titulo: "Letras - Licenciatura", 
-            desc: "Letras com ênfase em literatura e linguística.", 
-            img: "",
-            url: "https://app.faesma.com.br/captacao/public/selecao-curso"
-        }
-    ],
+            _graduacao: [
+                {
+                    titulo: "História - Licenciatura",
+                    desc: "Curso de História com foco em metodologias ativas.",
+                    img: "",
+                    url: "https://app.faesma.com.br/captacao/public/selecao-curso"
+                },
+                {
+                    titulo: "Pedagogia - Licenciatura",
+                    desc: "Formação completa em Pedagogia, para formar professores renomados.",
+                    img: "img/PEDAGOGIA.png",
+                    url: "https://app.faesma.com.br/captacao/public/selecao-curso"
+                },
+                {
+                    titulo: "Educação Especial - Licenciatura",
+                    desc: "Educação Especial com ênfase em inclusão social.",
+                    img: "",
+                    url: "https://app.faesma.com.br/captacao/public/selecao-curso"
+                },
+                {
+                    titulo: "Educação Física - Bacharelado",
+                    desc: "Educação Física para formação de profissionais capacitados.",
+                    img: "",
+                    url: "https://app.faesma.com.br/captacao/public/selecao-curso"
+                },
+                {
+                    titulo: "Letras - Licenciatura",
+                    desc: "Letras com ênfase em literatura e linguística.",
+                    img: "",
+                    url: "https://app.faesma.com.br/captacao/public/selecao-curso"
+                }
+            ],
+            get graduacao() {
+                return this._graduacao;
+            },
+            set graduacao(value) {
+                this._graduacao = value;
+            },
 
-    // Cursos de Pós-Graduação EAD
-    posGraduacao: [
-        { 
-            titulo: "Enfermagem do trabalho - Pós-Graduação", 
-            desc: "Especialização em Enfermagem do Trabalho.", 
+            _posGraduacao: [
+                {
+                    titulo: "Enfermagem do trabalho - Pós-Graduação",
+                    desc: "Especialização em Enfermagem do Trabalho.",
+                    img: "",
+                    url: "https://app.faesma.com.br/captacao/public/selecao-curso"
+                },
+                {
+                    titulo: "Psicopedagogia e Ludopedagogia - Pós-Graduação",
+                    desc: "Especialização em Psicopedagogia e Ludopedagogia.",
+                    img: "",
+                    url: "https://app.faesma.com.br/captacao/public/selecao-curso"
+                },
+                {
+                    titulo: "Educação Física Escolar e Interdisciplinaridade - Pós-Graduação",
+                    desc: "Especialização em Educação Física Escolar e Interdisciplinaridade.",
+                    img: "",
+                    url: "https://app.faesma.com.br/captacao/public/selecao-curso"
+                },
+                {
+                    titulo: "Neurociência com ênfase em Música - Pós-Graduação",
+                    desc: "Especialização em Neurociência com ênfase em Música.",
+                    img: "",
+                    url: "https://app.faesma.com.br/captacao/public/selecao-curso"
+                },
+                {
+                    titulo: "Educação Especial com ênfase em Transtorno do Espectro Autista (TEA) - Pós-Graduação",
+                    desc: "Especialização em Educação Especial com ênfase em TEA.",
+                    img: "",
+                    url: "https://app.faesma.com.br/captacao/public/selecao-curso"
+                },
+                {
+                    titulo: "Educação Especial e Inclusão - Pós-Graduação",
+                    desc: "Especialização em Educação Especial com ênfase em Inclusão.",
+                    img: "",
+                    url: "https://app.faesma.com.br/captacao/public/selecao-curso"
+                }
+            ],
+    get posGraduacao() {
+        return this._posGraduacao;
+    },
+    set posGraduacao(value) {
+        this._posGraduacao = value;
+    },
+    _tecnologo: [
+        {
+            titulo: "Logística - Tecnólogo",
+            desc: "Tecnologia e inovação em logística.",
             img: "",
             url: "https://app.faesma.com.br/captacao/public/selecao-curso"
         },
-        { 
-            titulo: "Psicopedagogia e Ludopedagogia - Pós-Graduação", 
-            desc: "Especialização em Psicopedagogia e Ludopedagogia.", 
+        {
+            titulo: "Processos Gerenciais - Tecnólogo",
+            desc: "Gestão de pessoas e empresas para o futuro.",
             img: "",
             url: "https://app.faesma.com.br/captacao/public/selecao-curso"
         },
-        { 
-            titulo: "Educação Física Escolar e Interdisciplinaridade - Pós-Graduação", 
-            desc: "Especialização em Educação Física Escolar e Interdisciplinaridade.", 
+        {
+            titulo: "Gestão de Recursos Humanos - Tecnólogo",
+            desc: "Gestão de pessoas para empresas de todos os tamanhos.",
             img: "",
             url: "https://app.faesma.com.br/captacao/public/selecao-curso"
         },
-        { 
-            titulo: "Neurociência com ênfase em Música - Pós-Graduação", 
-            desc: "Especialização em Neurociência com ênfase em Música.", 
+        {
+            titulo: "Gestão Ambiental - Tecnólogo",
+            desc: "Gestão ambiental com foco em sustentabilidade.",
             img: "",
             url: "https://app.faesma.com.br/captacao/public/selecao-curso"
         },
-        { 
-            titulo: "Educação Especial com ênfase em Transtorno do Espectro Autista (TEA) - Pós-Graduação", 
-            desc: "Especialização em Educação Especial com ênfase em TEA.", 
-            img: "",
-            url: "https://app.faesma.com.br/captacao/public/selecao-curso"
-        },
-        { 
-            titulo: "Educação Especial e Inclusão - Pós-Graduação", 
-            desc: "Especialização em Educação Especial com ênfase em Inclusão.", 
+        {
+            titulo: "Gestão Financeira - Tecnólogo",
+            desc: "Gestão financeira para empresas de todos os tamanhos.",
             img: "",
             url: "https://app.faesma.com.br/captacao/public/selecao-curso"
         }
     ],
-    // Cursos de Tecnólogo EAD
-    tecnologo: [
-        { 
-            titulo: "Logística - Tecnólogo", 
-            desc: "Tecnologia e inovação em logística.", 
-            img: "",
-            url: "https://app.faesma.com.br/captacao/public/selecao-curso"
-        },
-        { 
-            titulo: "Processos Gerenciais - Tecnólogo", 
-            desc: "Gestão de pessoas e empresas para o futuro.", 
-            img: "",
-            url: "https://app.faesma.com.br/captacao/public/selecao-curso"
-        },
-        { 
-            titulo: "Gestão de Recursos Humanos - Tecnólogo", 
-            desc: "Gestão de pessoas para empresas de todos os tamanhos.", 
-            img: "",
-            url: "https://app.faesma.com.br/captacao/public/selecao-curso"
-        },
-        { 
-            titulo: "Gestão Ambiental - Tecnólogo", 
-            desc: "Gestão ambiental com foco em sustentabilidade.", 
-            img: "",
-            url: "https://app.faesma.com.br/captacao/public/selecao-curso"
-        },
-        { 
-            titulo: "Gestão Financeira - Tecnólogo", 
-            desc: "Gestão financeira para empresas de todos os tamanhos.", 
-            img: "",
-            url: "https://app.faesma.com.br/captacao/public/selecao-curso"
-        }
-    ]
+    get tecnologo() {
+        return this._tecnologo;
+    },
+    set tecnologo(value) {
+        this._tecnologo = value;
+    },
 };
 
         function createCard(curso) {
